2e7bfb5db8587543de01649a5c96195baaff7280
Asi-phe <mazamisa.zisu@gmail.com>
2019-09-16 14:04:00 +0200

    adding a form

--- addsong.php
+++ addsong.php
@@ -3,12 +3,12 @@ require 'config/db.php';
 include 'inc/header.php';
 if(isset($_POST['submit'])){
     $title  =   $_POST['Title'];
-    $artist =   $_POST['Artist'];
+    $hotel =   $_POST['hotel'];
     $video_link =   $_POST['Video_link'];
     $rating =   $_POST['Rating'];
 
     //create query 
-    $query_insert   =   "INSERT INTO music_list(Title, Artist, Video_link, Rating) VALUES('$title','$artist','$video_link','$rating')";
+    $query_insert   =   "INSERT INTO music_list(Title, hotel, Video_link, Rating) VALUES('$title','$hotel','$video_link','$rating')";
 
     if(mysqli_query($conn,$query_insert)){
         header('Location: ' . 'index.php');
@@ -19,11 +19,11 @@ if(isset($_POST['submit'])){
 ?>
 <div class="container">
 <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-    <input type="text" name="Artist" placeholder="Artist" required>
+    <input type="text" name="hotel" placeholder="hotel" required>
     <input type="text" name="Title" placeholder="Title" required>
     <input type="text" name="Video_link" placeholder="Video URL" required>
     <input type="number" name="Rating" placeholder="Rating" min="0" max="5" required>
-    <button type="submit" name="submit">Add Song</button>
+    <button type="submit" name="submit">Book a Hotel</button>
 
 </form>
 </div>
