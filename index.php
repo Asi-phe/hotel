87cd61016d6a92990970beee7275a7674aaf4cd8
Asi-phe <mazamisa.zisu@gmail.com>
2019-09-18 09:58:09 +0200

    Adding new files

--- index.php
+++ index.php
@@ -1,105 +1,163 @@
-<?php 
+<?php
+//initialize the session
 session_start();
+ini_set('display_errors', 1);
 ?>
-<!DOCTYPE html>
+    <?php
+     require_once "connect.php";
+   
+     $sql= "CREATE TABLE IF NOT EXISTS bookings(
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstname VARCHAR(50),
+        surname VARCHAR(50),
+        hotel VARCHAR(30),
+        checkin VARCHAR(30),
+        checkout VARCHAR(30),
+        booked INT(4))";
+
+        $conn ->query($sql);
+        echo $conn-> error;
+    ?>
+
+    
+
+    <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/main.css">
-    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-     require_once "connect.php";
-    $sql = "CREATE TABLE IF NOT EXISTS bookings(
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        firstname VARCHAR(50),
-        lastname VARCHAR(50),
-        hotelname VARCHAR(50),
-        indate VARCHAR(30),
-        outdate VARCHAR(30),
-        booked INT(4))";
-  
-    $conn ->query($sql);
-    echo $conn-> error;
-    ?>
- 
-     <h1>TriggerTRIP</h1>
-        <form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
 
-<div>
-        <form>
-            <input type="text" id="firstname" name="firstname" placeholder="Your name.." required>
-            <input type="text" id="lastname" name="lastname" placeholder="Your last name.." required><br>
-            <input  type="date" id="In" name="indate" min="2018-01-01" max="2020-01-01" required><br>
-            <input  type="date" id="Out" name="outdate" min="2018-01-01" max="2020-01-01" required><br>
-</div>
-        
-        <select name="hotelname" required>
-            <option value="Hilton">Hilton</option>
-            <option value="Westin">Westin</option>
-            <option value="Four Seasons">Four Seasons</option>
-            <option value="Renaissance">Renaissance</option>
-        </select><br>
-        <input type="submit" name="submit"><input><br>
-   </form>
-   
-    <?php
-    //write to database
-    if(isset($_POST['submit'])){
-        //create Session var from poste data
-            $_SESSION['firstname'] = $_POST['firstname'];
-            $_SESSION['lastname'] = $_POST['lastname'];
-            $_SESSION['hotelname'] = $_POST['hotelname'];
-            $_SESSION['indate'] = $_POST['indate'];
-            $_SESSION['outdate'] = $_POST['outdate'];
-        }
-        //amount of days the user stays at the hotel
-        $datetime1 = new DateTime($_SESSION['indate']);
-        $datetime2 = new DateTime($_SESSION['outdate']);
-        $interval = $datetime1-> diff($datetime2);
+
+<form class="form-signin" role="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
+method="post">
+<h1 class="form-signin-heading">Enter your details for booking</h1>
+<label for = "firstname">First Name:</label><br>
+<input type="text" id="firstname" name="firstname" placeholder="First Name" require><br>
+<label for = "firstname">Surname:</label><br>
+<input type="text" id="surname" name="surname" placeholder="Surname" require><br>
+<label for="checkin">Checkin:</label><br>
+<input type="date" id="checkin" name="checkin" min="2019-05-03" max="2020-01-01"><br>
+<label for="checkin">Checkout:</label><br>
+<input type="date" id="checkout" name="checkout" min="2019-01-01" max="2020-01-01"><br>
+<label for="hotel">Hotel</label><br>
+<select name="hotel" required>
+<option value="Raddison Hotel">Raddison Hotel</option>
+<option value="Blue Moon">Blue Moon</option>
+<option value="Newtown Hotel">Newtown Hotel</option>
+<option value="The Panther ">The Panther</option>
+</select><br>
+
+<button type="submit" name="submit">Book Your Stay</button><br>
+</form>
+
+<?php
+
+if(isset($_POST['submit'])){
+//create a session variable from posted data
+    $_SESSION['firstname']=$_POST['firstname'];
+    $_SESSION['surname']=$_POST['surname'];
+    $_SESSION['hotel']=$_POST['hotel'];
+     $_SESSION['checkin']=$_POST['checkin'];
+    $_SESSION['checkout']=$_POST['checkout'];
+
+
+//amount of days the user stayed
+$datetime1 = new DateTime($_SESSION['checkin']);
+$datetime2 = new DateTime($_SESSION['checkout']);
+$interval = $datetime1->diff($datetime2);
+
+$checkInStamp = strtotime($_SESSION['checkin']);
+$checkOutStamp = strtotime($_SESSION['checkout']);
+// echo $checkInStamp . '<br>';
+// echo $checkOutStamp;
+if ($checkInStamp - $checkOutStamp > 86400 || $checkInStamp == $checkOutStamp) {
+    header("Location: ?error=timestamp");
+    exit;
+    }
+
+$interval->format('%d');
+
 $daysbooked = $interval->format('%d');
-    $value;
-switch($_SESSION['hotelname']){
-  case "Hilton":
-  $value = $daysbooked * 500;
+$value;
+
+switch(isset($_SESSION['hotel'])){
+  case 'Holiday Inn':
+  $value = $daysbooked * 2875;
   break;
-  case "Westin":
-  $value = $daysbooked * 600;
+
+  case 'Radison':
+  $value = $daysbooked * 1998;
   break;
-  case "Four Seasons":
-  $value = $daysbooked * 900;
+
+  case 'City Lodge':
+  $value = $daysbooked * 3765;
   break;
-  case "Renaissance":
-  $value = $daysbooked * 800;
+
+  case 'Town Lodge':
+  $value = $daysbooked * 7651;
   break;
+
   default:
-  return "Invalid Booking";
+  return "ERROR!";
 }
+//check for duplicate bookings///
+/*function duplicates ($param1, $param2){
+    $this ->firstname = $param1;
+    $this ->surname = $param2;
+}*/
+
+//create source
+/*if ($result = $this->conn->query("SELECT hotel, checkin, checkout FROM $this->tableName
+WHERE firstname = '$this->firstname' && surname = '$this -> surname'"))*/
+
+//display booking info for user
 echo "<div class='feedback'> <br> Firstname: ". $_SESSION['firstname'] . "<br>
-    Lastname: " . $_SESSION['lastname'].
-    "<br> Start Date: " . $_SESSION['indate'].
-    "<br> End Date: " . $_SESSION['outdate'].
-    "<br> Hotel Name: " . $_SESSION['hotelname'].
-    "<br>" . $interval->format('%R%a days') . "<br> total: " . $value . "</div>";
-        echo "<form class='form-inline' role='form' method='post' action=".
-        htmlentities($_SERVER["PHP_SELF"]).
-        "><input type='submit' name='confirm'></form>";
-        if(isset($_POST['confirm'])){
-            $stmt = $conn->prepare("INSERT INTO bookings(firstname,lastname,hotelname,indate,outdate)VALUES(?,?,?,?,?)");
-                $stmt -> bind_param('sssss',$firstname,$lastname,$hotelname,$indate,$outdate);
-                
-        
-        $firstname = $_SESSION['firstname'];
-        $lastname = $_SESSION['lastname'];
-        $hotelname = $_SESSION['hotelname'];
-        $indate = $_SESSION['indate'];
-        $outdate = $_SESSION['outdate'];
-            $stmt -> execute();
-                echo "booking confirmed";
-        }
+    Surname: " . $_SESSION['surname'].
+    "<br> Checkin: " . $_SESSION['checkin'].
+    "<br> Checkout: " . $_SESSION['checkout'].
+    "<br> Hotel: " . $_SESSION['hotel'].
+    "<br> Total: R " . $value . "</div>";
+     
+
+    echo "<form role='form' action=" . htmlspecialchars($_SERVER['PHP_SELF']) . " method='post'>
+    <button name='confirm' type='submit'> Confirm </button> </form>".'</div>';
+
+}
+
+
+if(isset($_POST['confirm'])){
+    $stmt = $conn ->prepare("INSERT INTO bookings(firstname,surname,hotel,checkin,checkout)
+    VALUES(?, ?, ?, ?, ?)");
+    $stmt -> bind_param('sssss',$firstname,$surname,$hotel,$checkin,$checkout);
+
+    //create a session variable from posted data
+    $firstname = $_SESSION['firstname'];
+    $surname = $_SESSION['surname'];
+    $hotel = $_SESSION['hotel'];
+    $checkin = $_SESSION['checkin'];
+    $checkout = $_SESSION['checkout'];
+    $stmt -> execute();
+        echo "BOOKING CONFIRMED!";
+}
+
 ?>
+<?php
+        if (isset($_GET['error']) && $_GET['error'] == 'timestamp') {
+    ?>
+        <div class='panel panel-default'>
+            <h3>
+                You must select at least  1 day 
+            </h3>
+            </div>
+    <?php
+        }
+    ?>
+
+
 </body>
-</html
+</html>
