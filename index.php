c1460ad1569b6b85a2a5680d4874b32fe9104bfa
Asi-phe <mazamisa.zisu@gmail.com>
2019-09-19 10:11:45 +0200

    Styling the heading

--- index.php
+++ index.php
@@ -1,170 +1,205 @@
 <?php
+// Start the session
 session_start();
 ?>
 
+
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
-          <!-- css link -->
-          <link rel="stylesheet" type="text/css" href="css/main.css">
-        <!-- font link -->
-    <link href="https://fonts.googleapis.com/css?family=Concert+One|Source+Sans+Pro&display=swap" rel="stylesheet"> 
-        <!-- bootstrap link -->
-    
-    <title>Bookings</title>
-</head>
+<html>
 <body>
-    <?php
-    require_once "connect.php";
+<link rel="stylesheet" href="css/main.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
-    $sql = "CREATE TABLE bookings (
-        firstname VARCHAR(50),
-        surname VARCHAR(50),
-        hotelname VARCHAR(50),
-        indate VARCHAR(30),
-        outdate VARCHAR(30),
-        booked INT(4)
-    )";
+<h1>Book Hotel</h1>
+<div class="price">
+<p id="inn">Holiday Inn: R 200</p>
+<p id="rad">Radison: R 100</p>
+<p id="city">City Lodge: R 400</p>
+<p id="town">Town Lodge: R 150</p>
+</div>
 
-    $conn->query($sql);
-    // echo $conn->error;
-    ?>
-<h1>Hotel booking</h1>
-<main class="container hotel">
+<div id='form'>
+<form role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
 
-  
-                <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" class="form">
-                <div class="form-group col-md-6">
-                        <label for="firstName">First name</label>
-                        <input type="text" class="form-control" placeholder="First name" name="firstname" required>
-                </div>
-                <br>
-                <div class="form-group col-md-6">
-                        <label for="surname">Last name</label>
-                        <input type="text" class="form-control" placeholder="Last name" name="surname" required>
-                </div>
-                <br>
-                <div class="form-group col-md-6">
-                <label for="hotels">Select a hotel below :</label>
-                <select class="form-control form-control-lg" name="hotelname" id="hotelName" required>
-                        <option value=" ">  </option>
-                        <option value="The Orleans - R700 per night">The Orleans - R700 per night</option>
-                        <option value="Bellagio - R600 per night">Bellagio - R600 per night </option>
-                        <option value="The Four Queens - R700 per night">The Four Queens - R700 per night</option>
-                        <option value="The Cosmopolitan - R800 per night">The Cosmopolitan - R800 per night </option>
-                        
-                </select>
-                </div>
-                <br>
-                <br>
-                <div class="form-group col-md-6">
-                <label for="start">Start date :</label>
-                <input type="date" class="form-control" name="indate" min="2018-01-01" max="2020-12-31" aria-label="Todo Date" aria-describedby="button-addon2">
-                <label for="end">End date :</label>
-                <input type="date" class="form-control" name="outdate" min="2018-01-01" max="2020-12-31" aria-label="Todo Date" aria-describedby="button-addon2">
-             </div>
-                <!-- <div class="form-group">
-                        <label for="numberOfDays">NUMBER OF DAYS</label>
-                        <input type="number" class="form-control" placeholder="Number Of Days" min=1 name="numberOfDays" id="nums" required>
-                </div> -->
-                <br>
-                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
-                </form>
-                <br>
+<label>First Name<input type="text" name="firstname" placeholder='First Name' required></label><br>
+<label>Surname<input type="text" name="surname"placeholder='surname' required></label><br>
+<label>Hotel Name
+<select name="hotelname" required>
+  <option value="Holiday Inn">Holiday Inn</option>
+  <option value="Radison">Radison</option>
+  <option value="City Lodge">City Lodge</option>
+  <option value="Town Lodge">Town Lodge</option>
+</select>
+</label><br>
 
-                
-                
-        
- 
+<label>In Date<input type="date" name="indate" placeholder='indate' required></label><br>
+<label>Out Date<input type="date" name="outdate" placeholder='outdate' required></label><br>
+<button class="submit" name="submit" type="submit">Submit</button>
+
+
+</form>
+</div>
+
+
 <?php
-if (isset($_POST['submit'])){
-    $_SESSION['firstname'] = $_POST['firstname'];
-    $_SESSION['surname'] = $_POST['surname'];
-    $_SESSION['hotelname'] = $_POST['hotelname'];
-    $_SESSION['indate'] = $_POST['indate'];
-    $_SESSION['outdate'] = $_POST['outdate'];
+require_once "connect.php";
+echo $conn->error;
 
-    $datetime1 = new DateTime ($_SESSION['indate']);
-    $datetime2 = new DateTime ($_SESSION['outdate']);
-    $interval = $datetime1->diff($datetime2);
-    $daysBooked = $interval->format('%R%a days');
-    $value;
-
-    switch($_POST['hotelname']){
-        case "The Orleans - R700 per night " ;
-        echo "<img src='images/orleans.jpg' id='images' >" . "<img src='images/orleans2.jpg' id='images' >" ;
-        $value = $daysBooked * 700;
-        break;
-
-        case "Bellagio - R600 per night" :
-        $value = $daysBooked * 600;
-        echo "<img src='images/bellagio.jpg' id='images' >" . "<img src='images/bel2.jpg' id='images' >" ;
-        
-        break;
-
-        case "The Four Queens - R700 per night" :
-        echo "<img src='images/four.jpg' id='images' >" . "<img src='images/four2.jpg' id='images' >" ;
-        $value = $daysBooked * 700;
-        break;
-
-        case "The Cosmopolitan - R800 per night" :
-        // echo "<img src='images/cosmo.jpg' id='images' >" . "<img src='images/cosmo2.jpg' id='images' >" ;
-        $value = $daysBooked * 800;
-        break;
-
-        default :
-        echo "Invalid Booking";
-    }
-    $firstname = $_POST['firstname'];
-    $surname = $_POST['surname'];
-    $result = mysqli_query($conn,"SELECT hotelname, indate, outdate, firstname, surname FROM bookings WHERE firstname='$firstname' && surname='$surname'");
-    if ($result->num_rows > 0) {
-       while ($row = $result->fetch_assoc()) {
-    echo "<div class='duplicate'> You already have a booking. <br> Firstname: ". $row['firstname'] . "<br>
-    Lastname: " . $row['surname'].
-    "<br> Start Date: " . $row['indate'].
-    "<br> End Date: " . $row['outdate'].
-    "<br> Hotel Name: " . $row['hotelname'].
-    "<br>" . $interval->format('%r%a days') . "<br> Total: R " . $value ."</div>";
-       }
-    }
-    echo '<div class="return">'. "<br> Firstname:".  $_SESSION['firstname']."<br>".
-    "surname:".  $_SESSION['surname']."<br>".
-    "Start Date:". $_SESSION['indate']."<br>".
-    "End Date:". $_SESSION['outdate']."<br>".
-    "Hotel Name:". $_SESSION['hotelname']."<br>".
-    "Total R" . $value ;
-    echo "<form role='form' action=" . htmlspecialchars($_SERVER['PHP_SELF']) . " method='post'>
-    <button type='submit' class='btn btn-primary' name='confirm'>Confirm</button></form>".'</div>';
-    //echo "<form role='form' action=" . htmlspecialchars($_SERVER['PHP_SELF']) . " method='post'><input type='submit' name='confirm'>.'Confirm'.</form>";
-    }
-    if(isset($_POST['confirm'])){
-    //Preparing and binding a statement
-    //prepare is method, this way we only pass the query once and then the values
-    $stmt=$conn->prepare("INSERT INTO bookings(firstname,surname,hotelname,indate,outdate) VALUES (?,?,?,?,?)");
-    //also part of preparing & binding these values to the questions marks.
-    $stmt->bind_param('sssss', $firstname,$surname,$hotelname,$indate,$outdate);
-    $firstname=$_SESSION['firstname'];
-    $surname=$_SESSION['surname'];
-    $hotelname=$_SESSION['hotelname'];
-    $indate=$_SESSION['indate'];
-    $outdate=$_SESSION['outdate'];
-    $stmt->execute();
-    echo '<div id="confirmed">'."Booking successfully confirmed".'</div>';
-    }
-  
-    ?>
+$sql = "CREATE TABLE IF NOT EXISTS bookings (
+   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+   firstname VARCHAR(50),
+   surname VARCHAR(50),
+   hotelname VARCHAR(50),
+   indate VARCHAR(30),
+   outdate VARCHAR(30),
+   booked INT(4))";
 
 
-</main>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+$conn ->query($sql);
+
+if (isset($_GET['error']) && $_GET['error'] == 'timestamp') {
+
+   ?>
+
+<div class='panel panel-default'>
+   <h1>
+      You must select at least  1 day 
+   </h1>
+      </div>
+
+      <?php
+   }
+
+
+//echo '<br>'. $_POST['firstname'] .'<br>'. $_POST['lastname'].'<br>'.$_POST['hotelname'].'<br>'.$_POST['indate'].'<br>'. $_POST['outdate'];
+
+
+
+
+if (isset($_POST['submit'])) {
+   $_SESSION['firstname']= $_POST['firstname'];
+   $_SESSION['surname']= $_POST['surname'];
+   $_SESSION['hotelname']= $_POST['hotelname'];
+   $_SESSION['indate']= $_POST['indate'];
+   $_SESSION['outdate']= $_POST['outdate'];
+
+
+
+//echo $_SESSION['firstname'] //."<br>".  $_SESSION['lastname'] ."<br>".  $_SESSION['hotelname'] ."<br>". $_SESSION['indate'] ."<br>". $_SESSION['outdate']."<br>";
+
+
+//display booking info to user
+//
+
+
+//calculate duration of user's stay at hotel
+$datetime1 = new DateTime($_SESSION['indate']);
+$datetime2 = new DateTime($_SESSION['outdate']);
+$interval = $datetime1->diff($datetime2);
+
+$interval->format('%d');
+
+$checkInStamp = strtotime($_SESSION['indate']);
+        $checkOutStamp = strtotime($_SESSION['outdate']);
+        // echo $checkInStamp . '<br>';
+        // echo $checkOutStamp;
+        if ($checkInStamp - $checkOutStamp > 86400 || $checkInStamp == $checkOutStamp) {
+            header("Location: ?error=timestamp");
+            exit;
+        }
+
+$daysbooked = $interval->format('%d');
+$value;
+
+switch(isset($_SESSION['hotelname'])){
+   case 'Holiday Inn':
+   $value = $daysbooked * 200;
+   break;
+
+   case 'Radison':
+   $value = $daysbooked * 100;
+   break;
+
+   case 'City Lodge':
+   $value = $daysbooked * 400;
+   break;
+
+   case 'Town Lodge':
+   $value = $daysbooked * 150;
+   break;
+
+   default:
+   return "ERROR!";
+}
+
+$firstname = $_POST['firstname'];
+$surname = $_POST['surname'];
+
+$result = mysqli_query($conn,"SELECT hotelname, indate, outdate, firstname, surname FROM bookings WHERE firstname='$firstname' && surname='$surname'"); 
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {    
+ echo "<div class='duplicate'> You already have a booking. <br> Firstname: ". $row['firstname'] . "<br>
+Lastname: " . $row['surname'].
+"<br> Start Date: " . $row['indate'].
+"<br> End Date: " . $row['outdate'].
+"<br> Hotel Name: " . $row['hotelname'].
+"<br>" . $interval->format('%r%a days') . "<br> Total: R " . $value ."</div>";
+    } 
+}
+
+
+echo '<div class="return">'. "<br> Firstname:".  $_SESSION['firstname']."<br>".
+"surname:".  $_SESSION['surname']."<br>".
+"Start Date:". $_SESSION['indate']."<br>".
+"End Date:". $_SESSION['outdate']."<br>".
+"Hotel Name:". $_SESSION['hotelname']."<br>".
+"<br>" . $interval->format('%r%a days') .
+"<br>".
+"Total R" . $value ;
+
+echo "<form role='form' action=" . htmlspecialchars($_SERVER['PHP_SELF']) . " method='post'>
+<button name='confirm' type='submit'> Confirm </button> </form>".'</div>';
+
+//echo "<form role='form' action=" . htmlspecialchars($_SERVER['PHP_SELF']) . " method='post'><input type='submit' name='confirm'>.'Confirm'.</form>";
+
+}
+
+if(isset($_POST['confirm'])){
+//Preparing and binding a statement
+//prepare is method, this way we only pass the query once and then the values
+$stmt=$conn->prepare("INSERT INTO bookings(firstname,surname,hotelname,indate,outdate) VALUES (?,?,?,?,?)");
+//also part of preparing & binding these values to the questions marks.
+$stmt->bind_param('sssss', $firstname,$surname,$hotelname,$indate,$outdate);
+$firstname=$_SESSION['firstname'];
+$surname=$_SESSION['surname'];
+$hotelname=$_SESSION['hotelname'];
+$indate=$_SESSION['indate'];
+$outdate=$_SESSION['outdate'];
+$stmt->execute();
+echo '<div id="confirmed">'."Booking confirmed".'</div>';
+
+}
+
+// if($_POST['confirm']){
+//    $firstname =$_SESSION['firstname'];
+//    $surname =$_SESSION['surname'];
+//    $hotelname =$_SESSION['hotelname'];
+//    $indate=$_SESSION['indate'];
+//    $outdate=$_SESSION['outdate'];
+//      mysqli_query($conn, "INSERT INTO bookings (firstname, surname, hotelname,indate,outdate)
+//      VALUES ('$firstname ', '$surname','$hotelname','$indate','$outdate')");
+// }
+
+
+
+
+?>
+
+
+
+
+
 </body>
 </html>
-  
